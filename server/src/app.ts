c53ad99a16f15cb467c514f7620e7faca619ed37
Your Name <you@example.com>
2025-07-25 16:05:11 +0300

    fixing errors

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+app.use('/api/auth', authRoutes); 
 app.use("/api/posts", postRoutes);
 app.use("/api/user", userRoutes);
 
